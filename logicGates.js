e5e1865170cf69fcf66267a632f8ed16abd8157b
Frank Schulz <Frank-Schulz@live.nmit.ac.nz>
2020-04-15 10:59:14 +1200

    pushing current work

---
+++ logicGates.js
@@ -0,0 +1,89 @@
+/*
+// And Or Not Gate function
+function OrAndNotGate(a, b, c) {
+    if (a == true || b == true) {
+        if (c != true) {
+            return true
+        } else return false
+    } else return false
+}
+
+console.log(OrAndNotGate(true, true, false))
+
+
+// And Or Not Gate function refactored
+function OrAndNotGate(a, b, c) {
+    if (typeof a === 'boolean' && typeof b === 'boolean' && typeof c === 'boolean') {
+        return ((a || b) && !c) ? true : false;
+    } else return "Not boolean"
+}
+
+console.log(OrAndNotGate(true, true, false))
+
+
+// And Or Gate function
+function AndOrGate(a, b, c) {
+    if (typeof a === 'boolean' && typeof b === 'boolean' && typeof c === 'boolean') {
+        return ((a && b) || c) ? true : false;
+    } else return "Input not boolean"
+}
+
+console.log(AndOrGate(true, false, true))
+
+
+// XOR Gate function
+function XORGate(a, b) {
+    if (typeof a === 'boolean' && typeof b === 'boolean') {
+        return (!(a && b) && (a || b)) ? true : false;
+        /*
+        y = !(a && b) ? true : false;
+        z = (a || b) ? true : false;
+        return (y && z) ? true : false;
+        *
+   } else return 'input not boolean'
+}
+
+let y = undefined;
+let z = undefined;
+
+console.log(XORGate(false, false))
+console.log(XORGate(false, true))
+console.log(XORGate(true, false))
+console.log(XORGate(true, true))
+*/
+
+/*
+A= [0,0,0,0,1,1,1,1]
+B= [0,0,1,1,0,0,1,1]
+C= [0,1,0,1,0,1,0,1]
+*/
+
+function truthTable(...args) {
+	columns = arguments.length
+    rows = Math.pow(2, columns)
+    console.log(columns, rows)
+
+	for (i = columns; i > 0; --i) {
+        console.log(i)
+        console.log(columnLetter = column[columnNum])
+        columnLetter = [];
+
+        for (i = rows; i > 0; --i) {
+            (i > (rows / 2)) ? columnLetter.push(0) : columnLetter.push(1);
+            }
+
+        console.log(column[columnNum], columnLetter)
+        // ++columnNum;
+        }
+        // return columnLetter
+    
+}
+
+
+let column = ['A', 'B', 'C', 'D', 'C', 'E', 'F']
+let columnNum = 0;
+
+let columns = 0;
+let rows = 0;
+
+console.log(truthTable(true, true, true))
